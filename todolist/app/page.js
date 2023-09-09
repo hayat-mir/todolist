962fa3d5e6d4c4ef77033898f818cdd0bd48a329
Hayat M <hayat28mir@gmail.com>
2023-09-09 12:46:50 +0530

    added files
    added all files for this project

---
+++ todolist/app/page.js
@@ -0,0 +1,77 @@
+"use client";
+import React, { useState } from "react";
+
+const page = () => {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMainTask([...mainTask, { title, desc }]);
+
+    settitle("");
+    setdesc("");
+  };
+  const deleteHandler = (i) => {
+    let copytask = [...mainTask];
+    copytask.splice(i, 1);
+    setMainTask(copytask);
+  };
+  let renderTask = <h2>No Task Available</h2>;
+
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li key={i} className="flex items-center justify-between mb-8 px-10 ">
+          <div className="flex items-center justify-between mb-5 w-2/3">
+            <h5 className="text-2xl font-semibold">{t.title}</h5>
+            <h6 className="text-xl font-semi-bold">{t.desc}</h6>
+          </div>
+          <button
+            onClick={() => {
+              deleteHandler(i);
+            }}
+            className="bg-red-400 text-white px-4 py-2"
+          >
+            Delete
+          </button>
+        </li>
+      );
+    });
+  }
+  return (
+    <>
+      <h1 className="bg-black text-white p-5 text-3xl font-bold text-center">
+        Hayat's TodoList
+      </h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2 "
+          placeholder="Enter Task Here"
+          value={title}
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2 "
+          placeholder="Enter Description Here"
+          value={desc}
+          onChange={(e) => {
+            setdesc(e.target.value);
+          }}
+        />
+        <button className="bg-black text-white m-5 px-4 py-3 text-2xl font-bold rounded">
+          Add Task
+        </button>
+      </form>
+      <hr />
+      <div className="p-8 bg-slate-200"></div>
+      <ul>{renderTask}</ul>
+    </>
+  );
+};
+
+export default page;
